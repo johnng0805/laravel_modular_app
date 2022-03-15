9697fa59edc7ea319823e25aefdcee4a088514a5
johnng0805 <johnng0805@gmail.com>
2022-03-15 20:36:14 +0700

    Add tailwind css.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './storage/framework/views/*.php',
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    './Modules/Auth/Resources/**/*.blade.php',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
